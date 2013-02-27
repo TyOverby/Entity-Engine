9599e8fc96d4185461597ac8095772fdd1826f71
TyOverby <ty@pre-alpha.com>
2013-02-26 22:25:25 -0800

    finished the handshake dsl

--- src/main/scala/com/prealpha/sge/networking/HandshakeGenerator.scala
+++ src/main/scala/com/prealpha/sge/networking/HandshakeGenerator.scala
@@ -1,47 +1,153 @@
 package com.prealpha.sge.networking
 
-import com.prealpha.sge.messages._
+import com.prealpha.sge.messages.{HandshakeMessage, Message}
+import java.net.Socket
+
+object HandshakeGenerator
 
 
-class Sequence(val prev: Option[Sequence] = None) {
+private[this] sealed trait NetOp
 
-  def <<[M <: Message, A] (d: M => Option[A]) = {
-    new IncomingSequence(d)(this)
-  }
+private[this] case class OutgoingMessage(m: Message) extends NetOp
 
-  def >> (m: => Message) = {
-    new OutgoingSequence(m)(this)
-  }
+private[this] case class IncomingMessage[M <: Message, A](d: M => Option[A]) extends NetOp
+private[this] case class NoCareIncoming[M <: Message, A](p: M => Option[A]) extends NetOp
+private[this] case class NoCareObject[M <: Message](o: M) extends NetOp
+
+object ConStarter {
+  implicit def conn2ConStarter(con: AbstractConnectionThread): ConStarter = ConStarter(con)
 }
 
+case object RECMESSAGE extends HandshakeMessage
 
+case class ConStarter(conn: AbstractConnectionThread) {
+  private[this] var ops = List[NetOp]()
 
-class StartingSequence(val connection: AbstractConnectionThread) extends Sequence(None)
-case object FakeStart extends Sequence(None)
-class OutgoingSequence(val m: Message)(p: Sequence) extends Sequence(Some(p))
-class IncomingSequence[M <: Message, A](val d: M => Option[A])(p: Sequence) extends Sequence(Some(p))
+  def >>(m: => Message) = {
+    ops = OutgoingMessage(m) :: ops
+    this
+  }
 
+  def <<[M <: Message, A](test: M => Option[A]) = {
+    ops = IncomingMessage(test) :: ops
+    this
+  }
+  def <<<[M <: Message, A](test: M => Option[A]) = {
+    ops = NoCareIncoming(test) :: ops
+    this
+  }
 
-object HandshakeGenerator {
-  implicit def act2seq(con: AbstractConnectionThread): StartingSequence = new StartingSequence(con)
+  def <<<[M <: Message](obj: M) = {
+    ops = NoCareObject(obj) :: ops
+    this
+  }
 
-  def reverse(s: Sequence) = {
-    def rev(s: Sequence, b: List[Sequence]): List[Sequence] = {
-      s match{
-        case s: StartingSequence      => b
-        case FakeStart                => b
-        case s: OutgoingSequence      => rev(s.prev.get, s::b)
-        case s: IncomingSequence[_,_] => rev(s.prev.get, s::b)
+  def apply(): List[Any] = {
+    def app(ops: List[NetOp]): List[Any] = {
+      ops match {
+        case Nil => Nil
+        case OutgoingMessage(m) :: xs => {
+          conn write m
+          conn.messagePublisher.blockUntilObject(RECMESSAGE)
+          app(xs)
+        }
+        case NoCareIncoming(d) :: xs => {
+          conn.messagePublisher.blockUnapply(d)
+          conn.write(RECMESSAGE)
+          app(xs)
+        }
+        case NoCareObject(o) :: xs => {
+          conn.messagePublisher.blockUntilObject(o)
+          conn.write(RECMESSAGE)
+          app(xs)
+        }
+        case IncomingMessage(d) :: xs => {
+          val rec = conn.messagePublisher.blockUnapply(d)
+          conn.write(RECMESSAGE)
+          rec :: app(xs)
+        }
       }
     }
-    rev(s, List[Sequence]())
+
+    app(ops.reverse)
   }
 }
 
-object Tester extends App{
-  import HandshakeGenerator._
+object Test extends App {
 
-  val x = FakeStart << SyncMessage.unapply >> SyncMessage(null,null)
-  println(reverse(x))
-  println(x.prev)
+  import ConStarter._
+
+  case class DoSomeExtracting(str: String, int: Int) extends HandshakeMessage
+
+  new AbstractConnectionThread(new Socket()) >> new HandshakeMessage {} >> new HandshakeMessage {} << DoSomeExtracting.unapply
 }
+
+//
+//import com.prealpha.sge.messages._
+//
+//
+//class NetOp(val prev: Option[Sequence] = None) {
+//
+//  def <<[M <: Message, A] (d: M => Option[A]) = {
+//    new IncomingSequence(d)(this)
+//  }
+//
+//  def >> (m: => Message) = {
+//    new OutgoingSequence(m)(this)
+//  }
+//}
+//
+//
+//trait StartignSequence extends Sequence{
+//  def write(m: Message)
+//  val messagePublisher: Publisher[Message]
+//}
+//
+//class StartSeq(val connection: AbstractConnectionThread) extends Sequence(None) with StartignSequence{
+//  def write(m: Message) {connection.write(m)}
+//  val messagePublisher = connection.messagePublisher
+//}
+//case object FakeStart extends Sequence(None) with StartignSequence{
+//  def write(m: Message) {println("")}
+//  val messagePublisher = new Publisher[Message]()
+//}
+//class OutgoingSequence(val m: Message)(p: Sequence) extends Sequence(Some(p))
+//class IncomingSequence[M <: Message, A](val d: M => Option[A])(p: Sequence) extends Sequence(Some(p))
+//
+//
+//object HandshakeGenerator {
+//  implicit def act2seq(con: AbstractConnectionThread): StartSeq = new StartSeq(con)
+//
+//  def reverse(s: Sequence): List[Sequence] = {
+//    def rev(s: Sequence, b: List[Sequence]): List[Sequence] = {
+//      s match{
+//        case s: StartSeq      => b
+//        case FakeStart                => b
+//        case s: OutgoingSequence      => rev(s.prev.get, s::b)
+//        case s: IncomingSequence[_,_] => rev(s.prev.get, s::b)
+//      }
+//    }
+//    rev(s, List[Sequence]())
+//  }
+//
+//  def connection(s: Sequence): Sequence = {
+//    reverse(s).head.prev.get
+//  }
+//
+//  def apply(connection: StartSeq, cons: List[Sequence]): List[_] = {
+//    cons match {
+//      case Nil => List()
+//      case (out: OutgoingSequence) :: xs => connection.write(out.m); apply(connection, xs)
+//      case (in: IncomingSequence[_,_])  :: xs => connection.messagePublisher.blockUnapply(in.d) :: apply(connection,xs)
+//    }
+//  }
+//}
+//
+//object Tester extends App{
+//  import HandshakeGenerator._
+//
+//  val x = FakeStart << SyncMessage.unapply >> SyncMessage(null, null)
+//  println(reverse(x))
+//  println(connection(x))
+//  println(x.prev)
+//}
--- src/test/scala/com/prealpha/sge/networking/networking/messagetests/BlockUnapplyTest.scala
+++ src/test/scala/com/prealpha/sge/networking/networking/messagetests/BlockUnapplyTest.scala
@@ -3,28 +3,27 @@ package com.prealpha.sge.networking.networking.messagetests
 import java.net.Socket
 import com.prealpha.sge.messages.HandshakeMessage
 import com.prealpha.sge.networking.networking._
+import com.prealpha.sge.networking.ConStarter._
 
 
 object BlockUnapplyTest extends App {
 
   case object ImHereMessage extends HandshakeMessage
 
-  case object ReceivedMessage extends HandshakeMessage
-
   case class SomeDataHere(str: String, int: Int) extends HandshakeMessage
 
   case class MyClient(ip: String, port: Int) extends Client("localhost", 1111) {
-    write(ImHereMessage)
-    println(messagePublisher.blockUnapply(SomeDataHere.unapply))
+    val clienthandshake = this >> ImHereMessage << SomeDataHere.unapply << SomeDataHere.unapply
+    val rec = clienthandshake()
+    println("CLIENT: "+rec)
 
-    println(SomeDataHere.getClass)
-
+    val List((a: String, b: Int), (c:String, d: Int))  = rec
+    println(f"a: ${a}, b: ${b}, c: ${c}, d: ${d}")
   }
 
   case class MyServer(socket: Socket) extends Server(socket) {
-    messagePublisher.blockUntilObject(ImHereMessage)
-    write(ImHereMessage)
-    write(SomeDataHere("hi", 5))
+    val serverHandshake = this <<< ImHereMessage >> SomeDataHere("hi", 4) >> SomeDataHere("lol", 5)
+    serverHandshake()
   }
 
   val masterServer = new MasterServer(1111, MyServer.apply)
